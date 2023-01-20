06c3e3b5e6200185908b70b592dae3aaf53763b1
SaneManiacMG <mmoroane@hotmail.com>
2023-01-20 10:03:39 +0200

    Init Commit

---
+++ src/main/java/com/demo/BootDemoApp/BootDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.BootDemoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDemoAppApplication.class, args);
+	}
+
+}
